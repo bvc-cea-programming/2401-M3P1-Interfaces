81cc1601bd02b67f29f5683fd73e0929e42f0778
huoxinghua <68455980+huoxinghua@users.noreply.github.com>
2024-09-23 16:02:11 -0600

    tree can shake

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -5,9 +5,12 @@ public class Enemy : MonoBehaviour
     private float _health=100;
     private float _damageValue = 30;
 
+
+    
     public void Damage()
     {
         Debug.Log("Enemy damaged");
+      
     }
 
 }
--- Assets/Scripts/Tree.cs
+++ Assets/Scripts/Tree.cs
@@ -2,8 +2,17 @@ using UnityEngine;
 
 public class Tree : MonoBehaviour
 {
+    [SerializeField] private Animator shakeTree;
+    private void Start()
+    {
+        shakeTree = GetComponent<Animator>();
+        Debug.Log("shakeTree is "+ shakeTree);
+        ShakeTree();
+    }
     public void ShakeTree()
     {
         Debug.Log("Tree Shook");
+        
+        //shakeTree.Play();
     }
 }
