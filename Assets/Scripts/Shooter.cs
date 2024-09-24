60ce75271dd9c33d910f460e7e4bc8077dba3c4e
Nibour16 <ruobinh3@hotmail.com>
2024-09-23 18:18:37 -0600

    Lab Complete

--- Assets/Scripts/Shooter.cs
+++ Assets/Scripts/Shooter.cs
@@ -21,21 +21,6 @@ public class Shooter : MonoBehaviour
     private void ExecuteCommand(RaycastHit hit)
     {
         Debug.Log(hit.collider.gameObject.name);
-        /*if (hit.collider.gameObject.GetComponent<Enemy>() != null)
-        {
-            Enemy enemy = hit.collider.gameObject.GetComponent<Enemy>();
-            enemy.Damage();
-        }
-        if (hit.collider.gameObject.GetComponent<Barrell>() != null)
-        {
-            Barrell barrell = hit.collider.gameObject.GetComponent<Barrell>();
-            barrell.Explode();
-        }
-        if (hit.collider.gameObject.GetComponent<Tree>() != null)
-        {
-            Tree tree = hit.collider.gameObject.GetComponent<Tree>();
-            tree.ShakeTree();
-        }*/
 
         if (hit.collider.gameObject.GetComponent<IShootable>() != null)
         {
